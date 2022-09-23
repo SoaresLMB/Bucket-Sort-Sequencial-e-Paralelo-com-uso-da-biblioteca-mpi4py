0da282537a62f58e516d3f7b835ee0628930f7fc
Leandro Soares <98189939+SoaresLMB@users.noreply.github.com>
2022-09-23 15:33:23 -0300

    Update algoritmo_bucketsort_em_paralelo.py

--- algoritmo_bucketsort_em_paralelo.py
+++ algoritmo_bucketsort_em_paralelo.py
@@ -8,7 +8,6 @@ p = comunicador.Get_size()
 
 tempo_inicial = MPI.Wtime()
 
-#array = [42, 32, 33, 52, 37, 47, 51,78,21,12,49,53]
 random.seed(77)
 array = random.sample(range(1,50000000),20000000)
 n_baldes = p-1
@@ -22,33 +21,25 @@ if my_rank == 0:
     array_enviado = array
 
 array_recebido = comunicador.bcast(array_enviado,root=0)
-#print(f'O processo {my_rank} recebeu o valor: {array_recebido}\n')
+
 balde = []
 
 if my_rank != 0:
-    #print(f'balde do processo {my_rank}')
-
     for j in array_recebido:
-
 
         if my_rank == 1:
             valor_max_balde = math.ceil(v)
             valor_min_balde = 0
             if j >= valor_min_balde and j < valor_max_balde:
                 balde.append(j)
-
         else:
             valor_min_balde = MAXBALDE * (my_rank - 1)
             valor_max_balde = valor_min_balde + MAXBALDE
             if j >= valor_min_balde and j < valor_max_balde:
                 balde.append(j)
-
-    #print(f'o processo {my_rank} selecionou o balde {balde}')
-
+                
     for z in range(n_baldes):
         balde = sorted(balde)
-
-    #print(f'o processo {my_rank} organizou o balde {balde}')
 
 arrays = comunicador.gather(balde,root=0)
 
